7c8b3a334866b7cb7ed80d6c636d39fcb807dabe
SaimirKapaj <kapajsaimir@gmail.com>
2021-05-12 08:44:36 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ export const Header: React.FC<HeaderProps> = ({
               />
             </g>
           </svg>
-          <h1>Acme</h1>
+          <h1>Acmse</h1>
         </div>
         <div>
           {user ? (
